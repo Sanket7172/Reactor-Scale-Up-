aa3c9a91e7db7d5cafd6bcd0eac507d44123519a
Sanket7172 <sanket.bedmutha@heranba.com>
2026-09-08 17:06:03 +0530

    Refactor docstring in __init__.py
    Updated docstring format and removed unnecessary lines.

--- visualization/__init__.py
+++ visualization/__init__.py
@@ -1,11 +1,1 @@
-"""
-Visualization package.
-
-Only the current 3D reactor figure is exported.
-"""
-
-from .reactor_3d import create_reactor_figure
-
-__all__ = [
-    "create_reactor_figure",
-]
+# Visualization package
